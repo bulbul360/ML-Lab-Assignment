2c833594f34ec71978cbb72ef4de08c17c044f69
bulbul360 <41160621+bulbul360@users.noreply.github.com>
2018-07-15 11:20:03 +0600

    Add files via upload

---
+++ ML/HousePrice/HousePricePrediction.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import ensemble
+from sklearn import svm
+
+house_data = pd.read_csv("kc_house_data.csv")
+
+y_value = house_data['price']
+conv_dates = [1 if values == 2014 else 0 for values in house_data.date ]
+house_data['date'] = conv_dates
+train = house_data.drop(['id', 'price'],axis=1)
+
+x_train , x_test , y_train , y_test = train_test_split(train , y_value , test_size = 0.10,random_state =2)
+
+reg = LinearRegression()
+reg.fit(x_train,y_train)
+print(reg.score(x_test,y_test))
+
+gbr_obj = ensemble.GradientBoostingRegressor(n_estimators = 1000, max_depth = 5, min_samples_split = 2,
+          learning_rate = 0.1, loss = 'ls')
+gbr_obj.fit(x_train, y_train)
+gbr_obj.score(x_test,y_test)
+
+model = svm.SVR(kernel='rbf', C=10, gamma=0.0001)
+model.fit(x_train, y_train)
+print(model.score(x_test, y_test))
